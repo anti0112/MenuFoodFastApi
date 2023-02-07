743d82667c4904905f299bd3ef121c2a6a672722
Danil Galkin <pixell_tm@bk.ru>
2023-02-07 05:32:03 +0300

    change commit __init__.py

--- menu/db/models/__init__.py
+++ menu/db/models/__init__.py
@@ -1,6 +1,7 @@
-from .submenu import Submenu
-from .dish import Dish
-from .menu import Menu
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
 Base: DeclarativeMeta = declarative_base()
+
+from .submenu import Submenu
+from .dish import Dish
+from .menu import Menu
